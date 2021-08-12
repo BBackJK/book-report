08d9fbca7ad7fc906526e820539d8807dbe2611e
bbacks <wnsrud6839@gmail.com>
2021-08-12 16:49:31 +0900

    0812 commit

--- JS/youDon'tKnowJS/chapter4/03_test.js
+++ JS/youDon'tKnowJS/chapter4/03_test.js
@@ -2,3 +2,17 @@ var a = [1, 2];
 var b = [3, 4];
 
 console.log(a + b);
+
+console.log(a.toString());
+
+var d = [1, 2, function() { return '1'; }, [0]];
+
+console.log(d.toString());
+
+console.log("true".valueOf());
+console.log("true".toString());
+
+console.log(Boolean("true").valueOf());
+
+
+console.log("true" == true);
